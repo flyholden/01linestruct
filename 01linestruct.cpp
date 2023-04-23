9f77fad6adb7c56ef86bc1f8b3a08c7eb1aa9b0a
flyholden <33690835+flyholden@users.noreply.github.com>
2023-04-23 23:24:26 +0800

    添加项目文件。

---
+++ 01linestruct.cpp
@@ -0,0 +1,18 @@
+﻿// 01linestruct.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <list>
+
+
+using namespace std;
+//0101  初始化（建立空的顺序表）
+List *MakeEmpty()
+{
+    List Ptrl;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+ 
